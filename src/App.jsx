e6d16ee31a7cff9d327f0e3b4b4c9ec0bfdd71ab
minzuy <duybmse18042@fpt.edu.vn>
2025-02-22 22:32:20 +0700

    fifth commit

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App() {
       </div>
 
       <div className="birthday-card">
-        <h1>Chúc mừng sinh nhật</h1>
+        <h1>Chúc mừng sinh nhật !</h1>
         <h2>Happy Birthday !</h2>
       </div>
 
